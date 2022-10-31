5bf77d64bd31cb07732f60da22b083b76e75d327
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2022-10-31 15:48:06 +0000

    Register page - database connection

--- app/eHealthCorp/register.py
+++ app/eHealthCorp/register.py
@@ -1,22 +1,30 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template
 from flask import request
-from eHealthCorp import db
+import sqlite3
 
 register = Blueprint('register', __name__, template_folder='templates')
 
 
 @register.route('/register', methods=["GET", "POST"])
 def show():
+
+    #database connection
+    conn = sqlite3.connect('./instance/db.sqlite3')
+    cur = conn.cursor()
+
     if request.method == "POST":
         username = request.form["username"]
         email = request.form["email"]
         password = request.form["password"]
 
-        print(f"---→ {username}: {email} {password}")
-        db.engine.execute(
-            "INSERT INTO user (username, email, password) VALUES (?, ?, ?)", (username, email, password),
-        )
+        # print(f"NEW USER --- {username}, {email}, {password} ---")
+
+        cur.execute("INSERT INTO user (username, email, password) VALUES (?, ?, ?)", (username, email, password))
+        conn.commit()
+        conn.close()
+
+        # print("New user added to the database")
 
         return render_template("home.html")
 
-    return render_template("login.html")
+    return render_template("register.html")
