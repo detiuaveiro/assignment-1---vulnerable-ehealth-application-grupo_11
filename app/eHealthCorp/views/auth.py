49d792b0bdaaadbab42d2092358a8216ccd96477
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2022-11-12 19:06:27 +0000

    Update insecure authentication

--- app/eHealthCorp/views/auth.py
+++ app/eHealthCorp/views/auth.py
@@ -1,4 +1,4 @@
-from flask import (Blueprint, request, render_template, redirect, session, url_for,)
+from flask import (Blueprint, request, render_template, redirect, session, url_for)
 from eHealthCorp import get_conn
 
 auth = Blueprint('auth', __name__)
@@ -19,7 +19,7 @@ def register():
             conn.commit()
             conn.close()
         except:
-            return render_template("register.html", ctx={"error": True, "error_desc": "This email already exists!"})
+            return render_template("register.html", error=True)
         else:
             return redirect(url_for("auth.login"))
 
@@ -32,15 +32,18 @@ def register_doctor():
     if request.method == "POST":
         first_name = request.form["first_name"]
         last_name = request.form["last_name"]
-        specialization = request.form["specialization"]
+        speciality = request.form["speciality"]
         email = request.form["email"]
         password = request.form["password"]
 
-        conn, cur = get_conn()
-        cur.execute("INSERT INTO app_user (email, password_, first_name, last_name) VALUES (?, ?, ?, ?)", (email, password, first_name, last_name))
-        conn.commit()
-        conn.close()
-        
+        try:
+            conn, cur = get_conn()
+            cur.execute("INSERT INTO app_user (email, password_, first_name, last_name) VALUES (?, ?, ?, ?)", (email, password, first_name, last_name))
+            conn.commit()
+            conn.close()
+        except:
+            return render_template("register_doctor.html", error=True)
+
 
         conn, cur = get_conn()
         result = cur.execute("SELECT id FROM app_user WHERE email = ?", (email,)).fetchall()
@@ -48,7 +51,7 @@ def register_doctor():
 
 
         conn, cur = get_conn()
-        cur.execute("INSERT INTO doctor (id, specialization) VALUES (?, ?)", (result[0][0], specialization))
+        cur.execute("INSERT INTO doctor (id, speciality) VALUES (?, ?)", (result[0][0], speciality))
         conn.commit()
         conn.close()
 
@@ -59,9 +62,7 @@ def register_doctor():
 
 @auth.route('/login', methods=('GET', 'POST'))
 def login():
-
-    #if the user is already logged in
-    if session.get("session_data") != None:
+    if "session_data" in session:
         return redirect(url_for("index.show"))
 
     if request.method == "POST":
@@ -74,13 +75,22 @@ def login():
         conn.close()
 
         if user == None:
-            return render_template("login.html", ctx={"fail": True})
+            return render_template("login.html", error=True)
         else:
+            #check the user type
+            conn, cur = get_conn()
+            exists = cur.execute("SELECT * FROM doctor WHERE id = ?", (user[0],)).fetchone()
+            if exists == None:
+                type = "user"
+            else:
+                type = "doctor"
+
             session.clear()
             session_data = {
-                "user_id": user[0],
+                "id": user[0],
                 "first_name": user[3],
-                "last_name": user[4]
+                "last_name": user[4],
+                "type": type
             }
             session["session_data"] = session_data
 
--- app/eHealthCorp/views/index.py
+++ app/eHealthCorp/views/index.py
@@ -5,14 +5,4 @@ index = Blueprint('index', __name__, template_folder='templates')
 
 @index.route('/')
 def show():
-    ctx = {"user": False}
-
-    #get session info
-    if session.get("user_id"):
-        ctx["user_id"] = session.get("user_id")
-        ctx["first_name"] = session.get("first_name")
-        ctx["last_name"] = session.get("last_name")
-        ctx["role"] = session.get("role")
-
-    print(ctx)
-    return render_template('index.html', ctx=ctx)
+    return render_template('index.html')
