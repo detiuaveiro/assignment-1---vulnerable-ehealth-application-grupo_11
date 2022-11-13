a48c7bea7abb6982f30ae1ccf439a3ff244b45db
Leonardo Almeida <leonardoalmeida7@ua.pt>
2022-11-13 12:39:39 +0000

    .

--- app/eHealthCorp/views/make_appointment.py
+++ app/eHealthCorp/views/make_appointment.py
@@ -1,10 +1,17 @@
-from flask import Blueprint, render_template
-from flask import request
+from flask import (Blueprint, render_template, session, redirect, url_for, request)
 
 make_appointment = Blueprint("/make_appointment", __name__, template_folder="templates")
 
 @make_appointment.route("/make_appointment", methods=["GET", "POST"])
 def show():
-    # TODO verificar se esta logado
+    session_ = False
+    if 'session_data' in session:
+        session_ = session['session_data']['type']
+        id_ = session['session_data']['id']
+
+    if session_ != 'user':
+        return redirect(url_for('auth.login'))
+
+    
 
     return render_template("make_appointment.html")
