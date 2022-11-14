31406844f6d172faf5b76ee16b59e4033a733602
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2022-11-14 15:56:57 +0000

    Add services

--- app/eHealthCorp/views/about.py
+++
@@ -1,7 +0,0 @@
-from flask import (Blueprint, render_template, session)
-
-about = Blueprint('/about', __name__, template_folder='templates')
-
-@about.route('/about')
-def show():
-    return render_template('about.html', title='About')
